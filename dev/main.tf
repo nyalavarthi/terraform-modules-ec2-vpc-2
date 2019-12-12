06b718532502fd9b53a091ade2648725dd4ce3bd
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-12 10:34:41 -0500

    disabled VPC

--- dev/main.tf
+++ dev/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+/*
 module "my_vpc" {
   source      = "../modules/vpc"
-  vpc_cidr    = "192.168.0.0/16"
+  vpc_cidr    = "172.31.0.0/16"
   tenancy     = "default"
   vpc_id      = "${module.my_vpc.vpc_id}"
-  subnet_cidr = "192.168.1.0/24"
+  subnet_cidr = "172.31.0.0/16"
 }
-
+*/
 module "my_ec2" {
   source        = "../modules/ec2"
   ec2_count     = 1
