876cd90b5f6662f6f243f5d153615a99dd53ccaf
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-12 10:39:14 -0500

    Hard coded subnet id

--- dev/main.tf
+++ dev/main.tf
@@ -15,5 +15,5 @@ module "my_ec2" {
   ec2_count     = 1
   ami_id        = "ami-5a8da735"
   instance_type = "t2.micro"
-  subnet_id     = "${module.my_vpc.subnet_id}"
+  subnet_id     = "subnet-00e08f4c30ad51bf2"
 }
